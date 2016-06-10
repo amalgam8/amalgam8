eb1328894ff572f687c1162b2c54c0cf77d1d129
Shriram Rajagopalan <shriram@us.ibm.com>
2016-06-10 14:05:03 -0400

    update k8s standalone installation script

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -14,42 +14,32 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-
-
-cat >/tmp/k8s.yml <<EOF
-etcd:
-  image: gcr.io/google_containers/etcd:2.0.12
-  net: "host"
-  command: /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
-master:
-  image: gcr.io/google_containers/hyperkube:v1.1.1
-  net: "host"
-  pid: "host"
-  privileged: true
-  ports:
-   - "8080:8080"
-  volumes:
-  - /:/rootfs:ro
-  - /sys:/sys:ro
-  - /dev:/dev
-  - /var/lib/docker/:/var/lib/docker:ro
-  - /var/lib/kubelet/:/var/lib/kubelet:rw
-  - /var/run:/var/run:rw
-  command: /hyperkube kubelet --containerized --hostname_override=127.0.0.1 --address=0.0.0.0 --api_servers=http://0.0.0.0:8080 --enable_server --config=/etc/kubernetes/manifests
-proxy:
-  image: gcr.io/google_containers/hyperkube:v1.1.1
-  net: "host"
-  privileged: true
-  command: /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
-EOF
-
-# Install/Run kubernetes
-docker-compose -f /tmp/k8s.yml up -d
-
-# Make API server accessible on host OS
+export K8S_VERSION="v1.2.3"
+export ARCH=amd64
+docker run \
+    --volume=/:/rootfs:ro \
+    --volume=/sys:/sys:ro \
+    --volume=/var/lib/docker/:/var/lib/docker:rw \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+    --volume=/var/run:/var/run:rw \
+    --net=host \
+    --pid=host \
+    --privileged=true \
+    --name=kubelet \
+    -d \
+    gcr.io/google_containers/hyperkube-${ARCH}:${K8S_VERSION} \
+    /hyperkube kubelet \
+        --containerized \
+        --hostname-override="127.0.0.1" \
+        --address="0.0.0.0" \
+        --api-servers=http://0.0.0.0:8080 \
+        --config=/etc/kubernetes/manifests \
+        --allow-privileged=true --v=2
+# ##Make API server accessible on host OS
 sleep 10
-docker exec tmp_master_1 sed -i 's/address=127.0.0.1/address=0.0.0.0/' /etc/kubernetes/manifests/master.json
-docker restart tmp_master_1
+docker exec kubelet perl -pi -e 's/address=127.0.0.1/address=0.0.0.0/' /etc/kubernetes/manifests/master.json
+docker restart kubelet
 
 # Install kubernetes CLI
-sudo curl -L http://storage.googleapis.com/kubernetes-release/release/v1.1.1/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
+sudo curl -L http://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/${ARCH}/kubectl > /usr/local/bin/kubectl
+sudo chmod +x /usr/local/bin/kubectl

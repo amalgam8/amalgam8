fc47b0d801a04e539759de5ff89fdc840a63f990
Isaiah Snell-Feikema <ijsnellf@us.ibm.com>
2016-06-10 09:48:38 -0500

    Added copyright header *.go

--- utils/health/doc.go
+++ utils/health/doc.go
@@ -1,3 +1,17 @@
+// Copyright 2016 IBM Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 // Package health provides functionality to add application-level health checks to individual application components.
 // Health check results can be exposed using an HTTP route and handler.
 // Components are expected to register health checkers, either as Checker interfaces or CheckerFunc using Register()

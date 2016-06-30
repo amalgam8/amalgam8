ff88616991c8fe98fe67f643f892620ea025d860
Shriram Rajagopalan <shriram@us.ibm.com>
2016-06-30 10:27:34 +0900

    renaming splib to version_routing

--- dynamicupdate/lua/version_routing.lua
+++ dynamicupdate/lua/version_routing.lua
@@ -2,16 +2,12 @@
 Amalgam8 version routing library
 ]]
 
-function get_upstream(service, default_version, version_selectors)
-    local version = get_version(service, default_version, version_selectors)
-    -- ngx.log(ngx.DEBUG, "version is " .. version)
-    return service.. "_" .. version, path
-end
-
 function get_target(service, default_version, version_selectors)
     local version = get_version(service, default_version, version_selectors)
-    local path = string.sub(ngx.var.request_uri, string.len(service)+2)
-    return service .. "_" .. version .. path
+    -- ngx.log(ngx.DEBUG, "version is " .. version)
+    -- local path = string.sub(ngx.var.request_uri, string.len(service)+2)
+    -- return service .. "_" .. version .. path
+    return service .. "_" .. version
 end
 
 function get_version(service, default_version, version_selectors)

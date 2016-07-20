c2ed1bf494311f1383e881a817533f709080d8c6
Shriram Rajagopalan <shriram@us.ibm.com>
2016-07-20 10:31:18 -0400

    fix the v + version in a8sidecar tarball

--- install-a8sidecar.sh
+++ install-a8sidecar.sh
@@ -17,7 +17,7 @@
 set -x
 set -e
 
-A8SIDECAR_RELEASE=v0.2.0-alpha1
+A8SIDECAR_RELEASE=0.2.0-alpha1
 OPENRESTY_RELEASE=1.9.15.1
 FILEBEAT_RELEASE=1.2.2
 
@@ -26,7 +26,7 @@ apt-get -y update && apt-get -y install libreadline-dev libncurses5-dev libpcre3
 
 wget -O /tmp/openresty-${OPENRESTY_RELEASE}.tar.gz https://openresty.org/download/openresty-${OPENRESTY_RELEASE}.tar.gz
 wget -O /tmp/filebeat_${FILEBEAT_RELEASE}_amd64.deb https://download.elastic.co/beats/filebeat/filebeat_${FILEBEAT_RELEASE}_amd64.deb
-wget -O /tmp/a8sidecar-${A8SIDECAR_RELEASE}-linux-amd64.tar.gz https://github.com/amalgam8/sidecar/releases/download/v${A8SIDECAR_RELEASE}/a8sidecar-${A8SIDECAR_RELEASE}-linux-amd64.tar.gz
+wget -O /tmp/a8sidecar-v${A8SIDECAR_RELEASE}-linux-amd64.tar.gz https://github.com/amalgam8/sidecar/releases/download/v${A8SIDECAR_RELEASE}/a8sidecar-v${A8SIDECAR_RELEASE}-linux-amd64.tar.gz
 
 ##Install OpenResty
 adduser --disabled-password --gecos "" nginx
@@ -44,10 +44,10 @@ cd /tmp && \
 dpkg -i /tmp/filebeat_${FILEBEAT_RELEASE}_amd64.deb
 
 #Install Sidecar -- This should be in the end, as it overwrites nginx.conf, filebeat.yml
-tar -xzf /tmp/a8sidecar-${A8SIDECAR_RELEASE}-linux-amd64.tar.gz -C /
+tar -xzf /tmp/a8sidecar-v${A8SIDECAR_RELEASE}-linux-amd64.tar.gz -C /
 
 #Cleanup
 rm -rf /tmp/openresty-${OPENRESTY_RELEASE}
 rm /tmp/openresty-${OPENRESTY_RELEASE}.tar.gz
 rm /tmp/filebeat_${FILEBEAT_RELEASE}_amd64.deb
-rm /tmp/a8sidecar-${A8SIDECAR_RELEASE}-linux-amd64.tar.gz
+rm /tmp/a8sidecar-v${A8SIDECAR_RELEASE}-linux-amd64.tar.gz

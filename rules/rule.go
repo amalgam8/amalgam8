ae31f26d8c9cc7c4c928b13e5500a9b651b64fce
Isaiah Snell-Feikema <ijsnellf@us.ibm.com>
2016-08-23 21:04:42 +0100

    Omit tags array if it is empty

--- rules/rule.go
+++ rules/rule.go
@@ -5,7 +5,7 @@ import "encoding/json"
 type Rule struct {
 	ID          string          `json:"id"`
 	Priority    int             `json:"priority"`
-	Tags        []string        `json:"tags"`
+	Tags        []string        `json:"tags,omitempty"`
 	Destination string          `json:"destination"`
 	Match       json.RawMessage `json:"match"`
 	Route       json.RawMessage `json:"route,omitempty"`

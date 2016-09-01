f958834b9e60edc19a7706f1ee5187ee5d64b1c7
Zvi Cahana <zcahana@gmail.com>
2016-09-01 10:26:40 +0300

    Add unit tests for configuration from flags, environment, and config gile

--- config/config_test.go
+++ config/config_test.go
@@ -18,6 +18,9 @@ import (
 	"os"
 	"time"
 
+	"fmt"
+	"io/ioutil"
+
 	"github.com/codegangsta/cli"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -44,6 +47,223 @@ var _ = Describe("Config", func() {
 			Expect(app.Run(os.Args[:1])).NotTo(HaveOccurred())
 		})
 
+		It("uses default config values", func() {
+			Expect(*c).To(Equal(DefaultConfig))
+		})
+
+	})
+
+	Context("config overiden with command line flags", func() {
+
+		BeforeEach(func() {
+			app := cli.NewApp()
+
+			app.Name = "sidecar"
+			app.Usage = "Amalgam8 Sidecar"
+			app.Flags = Flags
+			app.Action = func(context *cli.Context) {
+				c, _ = New(context)
+			}
+
+			args := append(os.Args[:1], []string{
+				"--register=true",
+				"--proxy=true",
+				"--service=helloworld:v1,somethingelse",
+				"--endpoint_host=localhost",
+				"--endpoint_port=9080",
+				"--endpoint_type=https",
+				"--registry_url=http://registry:8080",
+				"--registry_token=local",
+				"--registry_poll=5s",
+				"--controller_url=http://controller:6379",
+				"--controller_token=local",
+				"--controller_poll=5s",
+				"--supervise=true",
+				"--log=true",
+				"--logstash_server=logstash:8092",
+				"--log_level=debug",
+				"python", "productpage.py",
+			}...)
+
+			Expect(app.Run(args)).NotTo(HaveOccurred())
+		})
+
+		It("uses config values from command line flags", func() {
+			Expect(c.Register).To(Equal(true))
+			Expect(c.Proxy).To(Equal(true))
+			Expect(c.Service.Name).To(Equal("helloworld"))
+			Expect(c.Service.Tags).To(Equal([]string{"v1", "somethingelse"}))
+			Expect(c.Endpoint.Host).To(Equal("localhost"))
+			Expect(c.Endpoint.Port).To(Equal(9080))
+			Expect(c.Endpoint.Type).To(Equal("https"))
+			Expect(c.Registry.URL).To(Equal("http://registry:8080"))
+			Expect(c.Registry.Token).To(Equal("local"))
+			Expect(c.Registry.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Controller.URL).To(Equal("http://controller:6379"))
+			Expect(c.Controller.Token).To(Equal("local"))
+			Expect(c.Controller.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Supervise).To(Equal(true))
+			Expect(c.App).To(Equal([]string{"python", "productpage.py"}))
+			Expect(c.Log).To(Equal(true))
+			Expect(c.LogstashServer).To(Equal("logstash:8092"))
+			Expect(c.LogLevel).To(Equal("debug"))
+		})
+	})
+
+	Context("config overiden with environment variables", func() {
+
+		BeforeEach(func() {
+			app := cli.NewApp()
+
+			app.Name = "sidecar"
+			app.Usage = "Amalgam8 Sidecar"
+			app.Flags = Flags
+			app.Action = func(context *cli.Context) {
+				c, _ = New(context)
+			}
+
+			os.Setenv("A8_REGISTER", "true")
+			os.Setenv("A8_PROXY", "true")
+			os.Setenv("A8_SERVICE", "helloworld:v1,somethingelse")
+			os.Setenv("A8_ENDPOINT_HOST", "localhost")
+			os.Setenv("A8_ENDPOINT_PORT", "9080")
+			os.Setenv("A8_ENDPOINT_TYPE", "https")
+			os.Setenv("A8_REGISTRY_URL", "http://registry:8080")
+			os.Setenv("A8_REGISTRY_TOKEN", "local")
+			os.Setenv("A8_REGISTRY_POLL", "5s")
+			os.Setenv("A8_CONTROLLER_URL", "http://controller:6379")
+			os.Setenv("A8_CONTROLLER_TOKEN", "local")
+			os.Setenv("A8_CONTROLLER_POLL", "5s")
+			os.Setenv("A8_SUPERVISE", "true")
+			os.Setenv("A8_LOG", "true")
+			os.Setenv("A8_LOGSTASH_SERVER", "logstash:8092")
+			os.Setenv("A8_LOG_LEVEL", "debug")
+
+			Expect(app.Run(os.Args[:1])).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.Unsetenv("A8_REGISTER")
+			os.Unsetenv("A8_PROXY")
+			os.Unsetenv("A8_SERVICE")
+			os.Unsetenv("A8_ENDPOINT_HOST")
+			os.Unsetenv("A8_ENDPOINT_PORT")
+			os.Unsetenv("A8_ENDPOINT_TYPE")
+			os.Unsetenv("A8_REGISTRY_URL")
+			os.Unsetenv("A8_REGISTRY_TOKEN")
+			os.Unsetenv("A8_REGISTRY_POLL")
+			os.Unsetenv("A8_CONTROLLER_URL")
+			os.Unsetenv("A8_CONTROLLER_TOKEN")
+			os.Unsetenv("A8_CONTROLLER_POLL")
+			os.Unsetenv("A8_SUPERVISE")
+			os.Unsetenv("A8_LOG")
+			os.Unsetenv("A8_LOGSTASH_SERVER")
+			os.Unsetenv("A8_LOG_LEVEL")
+		})
+
+		It("uses config values from environment variables", func() {
+			Expect(c.Register).To(Equal(true))
+			Expect(c.Proxy).To(Equal(true))
+			Expect(c.Service.Name).To(Equal("helloworld"))
+			Expect(c.Service.Tags).To(Equal([]string{"v1", "somethingelse"}))
+			Expect(c.Endpoint.Host).To(Equal("localhost"))
+			Expect(c.Endpoint.Port).To(Equal(9080))
+			Expect(c.Endpoint.Type).To(Equal("https"))
+			Expect(c.Registry.URL).To(Equal("http://registry:8080"))
+			Expect(c.Registry.Token).To(Equal("local"))
+			Expect(c.Registry.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Controller.URL).To(Equal("http://controller:6379"))
+			Expect(c.Controller.Token).To(Equal("local"))
+			Expect(c.Controller.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Supervise).To(Equal(true))
+			Expect(c.App).To(Equal([]string{"python", "productpage.py"}))
+			Expect(c.Log).To(Equal(true))
+			Expect(c.LogstashServer).To(Equal("logstash:8092"))
+			Expect(c.LogLevel).To(Equal("debug"))
+		})
+	})
+
+	Context("config overiden with configuration file", func() {
+
+		configFile := fmt.Sprintf("%s/%s", os.TempDir(), "sidecar-config.yaml")
+
+		BeforeEach(func() {
+			app := cli.NewApp()
+
+			app.Name = "sidecar"
+			app.Usage = "Amalgam8 Sidecar"
+			app.Flags = Flags
+			app.Action = func(context *cli.Context) {
+				c, _ = New(context)
+			}
+
+			configYaml := `
+register: true
+proxy: true
+
+service:
+  name: helloworld
+  tags:
+    - v1
+    - somethingelse
+
+endpoint:
+  host: localhost
+  port: 9080
+  type: http
+
+registry:
+  url:   http://registry:8080
+  token: local
+  poll:  5s
+
+controller:
+  url:   http://controller:6379
+  token: local
+  poll:  5s
+
+supervise: false
+app: [ "python", "productpage.py" ]
+
+log: true
+logstash_server: logstash:8092
+
+log_level: debug
+`
+			err := ioutil.WriteFile(configFile, []byte(configYaml), 0777)
+			Expect(err).NotTo(HaveOccurred())
+
+			args := []string{
+				"--config=config.yaml",
+			}
+
+			Expect(app.Run(args)).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.Remove(configFile)
+		})
+
+		It("uses config values from configuration file", func() {
+			Expect(c.Register).To(Equal(true))
+			Expect(c.Proxy).To(Equal(true))
+			Expect(c.Service.Name).To(Equal("helloworld"))
+			Expect(c.Service.Tags).To(Equal([]string{"v1", "somethingelse"}))
+			Expect(c.Endpoint.Host).To(Equal("localhost"))
+			Expect(c.Endpoint.Port).To(Equal(9080))
+			Expect(c.Endpoint.Type).To(Equal("https"))
+			Expect(c.Registry.URL).To(Equal("http://registry:8080"))
+			Expect(c.Registry.Token).To(Equal("local"))
+			Expect(c.Registry.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Controller.URL).To(Equal("http://controller:6379"))
+			Expect(c.Controller.Token).To(Equal("local"))
+			Expect(c.Controller.Poll).To(Equal(time.Duration(5) * time.Second))
+			Expect(c.Supervise).To(Equal(true))
+			Expect(c.App).To(Equal([]string{"python", "productpage.py"}))
+			Expect(c.Log).To(Equal(true))
+			Expect(c.LogstashServer).To(Equal("logstash:8092"))
+			Expect(c.LogLevel).To(Equal("debug"))
+		})
 	})
 
 	Context("config validation", func() {

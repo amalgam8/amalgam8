d47dbc5b1216ea7a7754745c068bc894916b3da1
Zijun Wang <zijun.wang@ibm.com>
2017-02-17 22:44:07 -0500

    sync with upstream

--- cli/utils/i18n_resources.go
+++ cli/utils/i18n_resources.go
@@ -83,8 +83,8 @@ func localesEnUsJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "locales/en-US.json", size: 7621, mode: os.FileMode(493), modTime: time.Unix(1486047776, 0)}
-
+	info := bindataFileInfo{name: "locales/en-US.json", size: 7621, mode: os.FileMode(509), modTime: time.Unix(1486068029, 0)}
+	
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
